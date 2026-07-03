32c1db14d63dc85d27951905a81c56e29d06d1e0
Thiago Loaiza <thiagolosizat123@gmail.com>
2026-07-03 11:55:11 -0300

    Avances del TP 1 del 3/7

---
+++ TP1-ThiagoLoaiza/TP1-ThiagoLoaiza/Archivo1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TP1_ThiagoLoaiza
+{
+    partial class Configuracion
+    {
+        private int cantidadfilas;
+        private int cantidadcolumnas;
+        private int velocidadcopos;
+
+        public int CantidadFilas
+        {
+            set { cantidadfilas = value; }
+            get { return cantidadfilas; }
+        }
+
+        public int CantidadColumnas
+        {
+            set { cantidadcolumnas = value; }
+            get{ return cantidadcolumnas; }
+        }
+
+        public int VelocidadCopos
+        {
+            set { velocidadcopos = value; }
+            get { return velocidadcopos; }
+        }
+
+        public Configuracion()
+        {
+            cantidadfilas = 30;
+            cantidadcolumnas = 7;
+            velocidadcopos = 1;
+            Console.WriteLine("Costructor Configuracion - Datos por defecto");
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        public Configuracion(int cantidadfilas, int cantidadcolumnas, int velocidadcopos)
+        {
+            this.cantidadfilas = cantidadfilas;
+            this.cantidadcolumnas = cantidadcolumnas;
+            this.velocidadcopos = velocidadcopos;
+        }
+
+        public int ReturnarColumnas()
+        {
+            return cantidadcolumnas;
+        }
+        public int ReturnarFilas()
+        {
+            return cantidadfilas;
+        }
+    }
+}
---
+++ TP1-ThiagoLoaiza/TP1-ThiagoLoaiza/Archivo2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TP1_ThiagoLoaiza
+{
+    //new Random().Next();
+    partial class CopoDeNieve : Configuracion
+    {
+        public string Modelo = "*";
+        public int posicionX;
+        public int posicionY;
+        public int limite;
+        public void Spawneo()
+        {
+            limite = CantidadFilas;
+            int posicionXAnterior = 0;
+            int RetrasoSpawn = 1000;
+            bool PosicionOcupada = true;
+            while (true)
+            {
+                Thread.Sleep(RetrasoSpawn / VelocidadCopos);
+                posicionX = new Random().Next(0, CantidadColumnas);
+                posicionY = new Random().Next(0, CantidadFilas);
+
+                while (PosicionOcupada)
+                {
+                    if (posicionX == posicionXAnterior)
+                    {
+                        posicionX = new Random().Next(0, CantidadColumnas);
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(posicionX, 0);
+                        Console.Write(Modelo);
+                        PosicionOcupada = false;
+                    }
+                    posicionXAnterior = posicionX;
+                }
+                PosicionOcupada = true;
+            }
+        }
+        public void Caida()
+        {
+        }
+    }
+}
